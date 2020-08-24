86dea75775105b01ab598554e8765ace729ebac4
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-24 23:56:32 +0700

    Hello World

---
+++ comment.py
@@ -0,0 +1,11 @@
+#This is a comment
+#This post will not be executed
+#comments with pound signs can only be used for one line
+"""
+Comment writing is more than one line ie
+by using double quotes 3 times and
+closed with double quotes 3 times too
+"""
+
+#print Hello World
+print("Hello World")
