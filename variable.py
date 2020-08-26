d6f5898fc9889642099aaa786dc0317da837f862
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-26 23:55:51 +0700

    variable

---
+++ variable.py
@@ -0,0 +1,22 @@
+a = 10
+x = 9
+
+long = 1000
+
+#first call
+print("Value a = ", a)
+print("Value b = ", x)
+print("Value long = ", long)
+
+#Naming
+value_y = 15 #with underscore
+number4 = 40000000 
+
+#Second Call
+print("Value a =", a)
+a = 7
+print("Value a =", a)
+
+#assignment indirect
+b = a
+print("Value b = ", b)
