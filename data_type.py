ff6700d734ce45ed8ee35c668e4694d862ac10dc
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-27 23:34:46 +0700

    type data

---
+++ data_type.py
@@ -0,0 +1,76 @@
+#float
+data_float = 7 
+print("data : ", data_float)
+print("- type : ", type(data_float))
+
+#string
+data_string = "Hello"
+print("data : ", data_string)
+print("- type : ", type(data_string))
+
+#boolean
+data_bool = False
+print("data : ", data_bool)
+print("- type : ", type(data_bool))
+
+#complex
+data_complex = complex(5,6)
+print("data : ", data_complex)
+print("- type : ", type(data_complex))
+
+#data type from C
+data_c_double = c_double(10.5)
+print("data : ", data_c_double)
+print("- type : ", type(data_c_double))
+
+#change data type
+
+#integer
+data_int = 5
+print("data = ", data_int, ", type =", type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int)
+print("data = ", data_float, ", type =" , type(data_float))
+print("data = ", data_str, ",type =", type(data_str))
+print("data = ", data_bool, ", type =", type(data_bool))
+
+##float
+data_float = 3.5
+print("data = ", data_float, ", type =", type(data_float))
+
+data_int = int(data_float)
+data_str = str(data_float)
+data_bool = bool(data_float)
+print("data = ", data_int, ",type =", type(data_int))
+print("data = ", data_str, ",type =" , type(data_str))
+print("data = ", data_bool, ",type =", type(data_bool))
+
+
+#boolean
+data_bool = 5;
+print("data = ", data_bool, ", type =", type(data_bool))
+
+data_int = int(data_bool)
+data_str = str(data_bool)
+data_bool = bool(data_bool)
+print("data = ", data_int, ",type =", type(data_int))
+print("data = ", data_str, ",type =" , type(data_str))
+print("data = ", data_bool, ",type =", type(data_bool))
+
+#string
+data_str = "3";
+print("data = ", data_str, ", type =", type(data_str))
+
+data_int = int(data_str)
+data_str = str(data_str)
+data_str = bool(data_str)
+print("data = ", data_int, ",type =", type(data_int))
+print("data = ", data_str, ",type =" , type(data_str))
+print("data = ", data_str, ",type =", type(data_str))
+
+
+
+
+
