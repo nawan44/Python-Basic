f20c96c111db7f9974bb67ad5107017fd976c322
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-28 12:59:06 +0700

    casting data

---
+++ casting_data.py
@@ -0,0 +1,18 @@
+#input user
+
+#string
+data = input ("Maskukkan data : ")
+print("data = ", data, ",type =", type(data))
+
+#integer
+
+data_int = int(input("masukkan angka: "))
+print("data =", data_int, ",type =", type(data_int))
+
+#flolat
+data_float = float(input("masukkan angka: "))
+print("data =", data_float, ",type =", type(data_float))
+
+#boolean
+biner =bool(int(input("masukkan nilai boolean:")))
+print("data =", biner, ",type =", type(biner))
