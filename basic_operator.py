759a076e395307bde0df401c7a63fddbc50a2f7f
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-08-30 23:30:04 +0700

    basic operator

---
+++ basic_operator.py
@@ -0,0 +1,45 @@
+#plus
+tambah = 1 + 5
+print(tambah)
+
+#minus
+kurang = 7-8
+print(kurang)
+
+#multiple
+kali = 2*3
+print(kali)
+
+#devide
+bagi = 12/4
+print(bagi)
+
+#combine
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+#mod
+mod = 11 % 3
+print(mod)
+
+#two multiplication
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+#string 
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+#multiplying strings
+lotsofhellos = "hai" * 10
+print(lotsofhellos)
+
+#Operators with Lists
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
