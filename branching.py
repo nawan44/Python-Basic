67efc21d2355945ecafbf5f89b6f84370b660de8
nawan44 <rachmat.d.gunawan@gmail.com>
2020-09-02 16:58:01 -0700

    iteration branching

---
+++ branching.py
@@ -0,0 +1,7 @@
+#basic branching
+fast = True
+if fast :
+    print('Just Walk')
+else :
+    print('sleep')
+
---
+++ iteration.py
@@ -0,0 +1,9 @@
+#basic iteration
+Spirit = 5
+for Day in range(1, Spirit+1):
+    print('Never Give Up')
+
+#iteration in string
+Hi = 7
+for Good in range(2, Hi+7):
+    print(f'Morning {Good}')
