6b1d1f7653a50d1e6881a9b93fa044acdfcef0d5
nawan44 <rachmat.d.gunawan@gmail.com>
2020-09-06 17:06:46 +0700

    logika operation

---
+++ logika_operation.py
@@ -0,0 +1,70 @@
+#NOT
+a = True
+c = not a
+
+print('data a =', a)
+print('data c =', c)
+
+#OR
+a = False
+b = False
+c = a or b
+print(a, 'OR', b, '=', c)
+
+a = False
+b = True
+c = a or b
+print(a, 'OR', b, '=', c)
+
+a = True
+b = False
+c = a or b
+print(a, 'OR', b, '=', c)
+
+a = True
+b = True
+c = a or b
+print(a, 'OR', b, '=', c)
+
+
+#AND
+a = False
+b = False
+c = a and b
+print(a, 'AND', b, '=', c)
+
+a = False
+b = True
+c = a and b
+print(a, 'AND', b, '=', c)
+
+a = True
+b = False
+c = a and b
+print(a, 'AND', b, '=', c)
+
+a = True
+b = True
+c = a and b
+print(a, 'AND', b, '=', c)
+
+#XOR
+a = False
+b = False
+c = a ^ b
+print(a, 'XOR', b, '=', c)
+
+a = False
+b = True
+c = a ^ b
+print(a, 'XOR', b, '=', c)
+
+a = True
+b = False
+c = a ^ b
+print(a, 'XOR', b, '=', c)
+
+a = True
+b = True
+c = a ^ b
+print(a, 'XOR', b, '=', c)
