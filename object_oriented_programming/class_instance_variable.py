9c41b7a86a598847e1b80476559a5da98951b096
Rachmat Gunawan <rachmat.d.gunawan@gmail.com>
2020-09-17 12:31:09 +0700

    class instance variable

---
+++ object_oriented_programming/class_instance_variable.py
@@ -0,0 +1,22 @@
+class Oop :
+    #class variable
+    con = 0
+    def __init__(self, stackTech, softSkill, scoreS):
+        self.tech = stackTech
+        self.softK = softSkill
+        self.score = scoreS
+	    Oop.con +=1
+        print("Make OOP with " + stackTech)
+
+Backend = Oop("Python", "Fast Learning", 100)
+print(Oop.con)
+Webd = Oop("php", "Work Hard", 80)
+print(Oop.con)
+Webk = Oop("ruby", "dilligent", 90)
+print(Oop.con)
+
+print(Backend.tech)
+print(Webd.softK)
+print(Webk.__dict__)
+
+
