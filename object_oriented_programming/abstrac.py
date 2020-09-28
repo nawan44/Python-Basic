c45ca6124817abc478494ab8c4f40e6ab7d60f23
nawan44 <rachmat.d.gunawan.com>
2020-09-28 23:40:15 +0700

    abstract

---
+++ object_oriented_programming/abstrac.py
@@ -0,0 +1,24 @@
+class Name :
+    def__init__(self, address, noHp, skill, age):
+        self.address = address
+        self.noHp = noHp
+        self.skill = skill
+        self.age = age
+    def frontend(self, level):
+        print(self.skill + ' Javascript' + level.address)
+        level.backend(self, self.noHp)
+    def backend(self):
+        print(self.skill + ' Java' + level.address)
+        
+rate = Name('rate', 80, 90, 70)
+Deploy = Name ('hours', 5, 6, 8)
+
+rate.backend()
+
+def function(argument1, argument2):
+    print(argument1)
+    print(argument2)
+
+print(rate)
+rate.backend()
+rate.frontend(hours)
