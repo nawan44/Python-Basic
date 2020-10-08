f7b82f673e09e37a7b3ad046470bd2629f401b79
nawan44 <rachmat.d.gunawan.com>
2020-10-09 00:12:47 +0700

    tkinter

--- object_oriented_programming/oop_tkinter.py
+++ object_oriented_programming/oop_tkinter.py
@@ -5,10 +5,15 @@ main_window = tkinter.Tk()
 print(main_window.__dict__)
 
 label1 = tkinter.Label(main_window, text = "label1")
-label1 = tkinter.Label(main_window, text = "label2")
+label2 = tkinter.Label(main_window, text = "label2")
 
+tombol1 = tkinter.Button(main_window, text = "tombol1")
+tombol2 = tkinter.Button(main_window, text = "tombol2")
 #method positioning
 label1.pack()
 label2.pack()
+tombol1.pack()
+tombol2.pack()
 
+#method menampilkan GUI
 main_window.mainloop()
